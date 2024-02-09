35bd69cc1a59a621b4b4c0a7c52e7d57554939b3
sanchyopas <saniagolovanev@gmail.com>
2024-02-09 22:32:26 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+import styled from 'styled-components';
+import Header from './layout/header/Header';
+
+
+function App() {
+    return (
+        <div className="App">
+            <Header></Header>
+        </div>
+    );
+}
+
+export default App;
